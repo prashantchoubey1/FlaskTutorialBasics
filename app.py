4b5f35de72b66eddcf0c6d3646a894c9ca1de20e
prashant.choubey@dpworld.com <prashantchoubey1@github.com>
2024-04-10 21:25:00 +0530

    all db files working fine

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ## Importing flask library - helps to build API in flask
-from flask import Flask, render_template
+from flask import Flask, g,render_template
 from flask import request
 
 ## creating flask application
@@ -10,10 +10,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# Print hi function
 @app.route("/hi")
 def abc():
     return "<p>hello guddu</p>"
-
 
 ## Create a python api to find whether a number is even or odd
 @app.route("/even_validation", methods=["GET","POST"])
@@ -26,70 +26,80 @@ def even_odd():
     
 @app.route("/loop", methods=["GET","POST"])
 def loop_func():
-    ls=[]
-    s=""
-    num=int(request.args.get('num'))
-    for i in range(0,num):
-        s=s+","+str(i)
-    return s
+    if(request.method=="POST"):
+        ls=[]
+        s=""
+        num=int(request.args.get('num'))
+        for i in range(0,num):
+            s=s+","+str(i)
+        return s
+    else:
+       return "No data"
 
-# @app.route("/search_form", methods=["GET","POST"])
-# def database_view():
-#     employee_id=int(request.form['employee_id'])
-#     query="select * from details where id=?"
-#     data=view_data(query, employee_id)
-#     return render_template('search.html', sql_query=query,data=data)
+@app.route("/search_form", methods=["GET","POST"])
+def database_view():
+    if(request.method=="POST"):
+        employee_id=int(request.form['employee_id'])
+        query="select * from details where id=?"
+        data=view_data(query, employee_id)
+        return render_template('search.html',data=data)
+    else:
+       return render_template('search.html')
+
 
 @app.route("/submit_form", methods=["GET","POST"])
 def database_insert():
-    employee_id=int(request.form['employee_id'])
-    employee_name=str(request.form['employee_name'])
-    query="INSERT INTO details (id, name) VALUES(?,?)"
-    status=insert_data(query, employee_id,employee_name)
-    print(status)
-    return render_template('home.html', 
-                           employee_id=employee_id, 
-                           employee_name=employee_name, 
-                           sql_query=query,
-                           status=status)
+    if(request.method=="POST"):
+        employee_id=int(request.form['employee_id'])
+        employee_name=str(request.form['employee_name'])
+        query="INSERT INTO details (id, name) VALUES(?,?)"
+        status=insert_data(query, employee_id,employee_name)
+        print(status)
+        return render_template('home.html', 
+                            employee_id=employee_id, 
+                            employee_name=employee_name, 
+                            sql_query=query,
+                            status=status)
+    else:
+       return render_template('home.html')
 
 
 import sqlite3 as sql
-def insert_data(query,id,name):  
-  try:
-    # Connecting to database
+def connect_db():
+   # Connecting to database
     print("connection initiating")
     con = sql.connect('database/employee.db')
     print("connection successful")
     # Getting cursor
     c =  con.cursor() 
     print("cursor created")
+    return c
+
+def insert_data(query,id,name):  
+  try:
+    #connect db
+    c=connect_db()
     # Adding data
     c.execute(query,(id,name) )
     print("query executed")
     # Applying changes
-    con.commit()
+    c.commit()
     print("query commited")
   except:
     print("An error has occured")
 
 def view_data(query,id):  
   try:
-    # Connecting to database
-    print("connection initiating")
-    con = sql.connect('database/employee.db')
-    print("connection successful")
-    # Getting cursor
-    c =  con.cursor() 
-    print("cursor created")
+    con=connect_db()
     # Adding data
-    c.execute(query,id)
-    rows = c.fetchall()
-    print("query executed")
-    return rows
+    g.db = con
+    cur = g.db.execute(query, [id])
+    data = [dict(id=row[0], name=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return data
   except:
     print("An error has occured")
-    
+
 
 # Main Driver Function 
 if __name__ == '__main__':
